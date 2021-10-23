a7a9ab6bbe2d4d58b0f112de8e7713024079c4cc
Ibrahim Sheikh <89422371+ibrahimsheikh89@users.noreply.github.com>
2021-10-23 13:12:48 -0400

    Update settings.py

--- mywebsiteProject/settings.py
+++ mywebsiteProject/settings.py
@@ -21,7 +21,7 @@ PROJECT_DIR = os.path.join(BASE_DIR, "mywebsiteProject")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yyidnidnuf+2@n2h055r+0*9#)yy+!lp#f-2yg228f*%u8x-jn'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

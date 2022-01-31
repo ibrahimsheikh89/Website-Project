56f3dce0b61bef28eaf4dd8a99393ea3122e51b4
Ibrahim Sheikh <89422371+ibrahimsheikh89@users.noreply.github.com>
2022-01-31 09:51:34 -0500

    Update urls.py

--- mywebsiteProject/apps/contact/urls.py
+++ mywebsiteProject/apps/contact/urls.py
@@ -3,4 +3,4 @@ from django.urls import path
 from .views import contact
 
 app_name = "contact"
-urlpatterns = [path("", contact, name="contact")]
+urlpatterns = [path("", contact, name="contact")] 

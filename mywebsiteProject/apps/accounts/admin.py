99a40e9ea400b866d6a66d1efd32b7eb3045dfbb
Ibrahim Sheikh <89422371+ibrahimsheikh89@users.noreply.github.com>
2022-02-03 14:07:27 -0500

    Update admin.py

--- mywebsiteProject/apps/accounts/admin.py
+++ mywebsiteProject/apps/accounts/admin.py
@@ -4,4 +4,5 @@ from .models import UserProfile, UserPersona, UserInterest
 
 admin.site.register(UserProfile)
 admin.site.register(UserPersona)
-admin.site.register(UserInterest) 
+admin.site.register(UserInterest)
+ 
